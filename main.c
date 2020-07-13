9424b14f739c533bf795a1963f4df95934e06ad1
Kampfkuchen <48136788+Battlecake91@users.noreply.github.com>
2020-07-14 01:35:27 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
-/*/*
+/*
  * Comfortblink_ATTiny13A.c
- * Version V1.2b
+ * Version V2.0b
  * Author : Steven Tzschentke (aka Kampfkuchen)
  * Initiated in 626ge.nufag.de board for Mazda 626 GE
  * Thread: http://www.626ge.de/viewtopic.php?f=17&t=5663
- * Changes:
- * V1.2b: fixed cycle-setup at normal blinking 
- * adding time-out
+ *
+ * Change-log:
+ * V1.2b:	fixed cycle-setup at normal blinking 
+ *			adding time-out
+ * V2.0b:	Both-detection now works with timer
+ *			Brownout-Detection at 2.7V
+ *			fix the forever-blinking-bug  
+ *			some optimizing with counters
+ *			(alternative cycle-timing)
  */ 
 
 #define F_CPU 9600000UL
@@ -20,11 +26,13 @@
 #define no_lever 0x00
 
 #define active 1
+#define inactive 0
+#define disabled 2
 
-#define time_in 40
-#define time_out 300
+#define time_in 30
+#define time_out 450
 #define time_out_Feedback 1000
-#define time_out_both 700
+#define time_out_both 20
 
 #include <avr/io.h>
 #include <avr/eeprom.h>
@@ -32,25 +40,24 @@
 #include <stdint.h>                            
 
 volatile uint16_t counter = 0;
-uint8_t detect_count = 0;
 uint8_t lever = 0;
 uint8_t Feedback = 0;
 uint8_t Feedback_last = 0;
 uint8_t Feedback_count = 0;
-uint8_t Feedback_count_last = 0;
 uint8_t state = 0;
 uint8_t comfort = 0;
 uint8_t cycles = 0;
 uint16_t counter_last_check = 0;
 uint16_t both_counter = 0;
 uint16_t time_last_Feedback = 0;
+uint16_t comfort_start_time = 0;
+uint16_t time_first_detection = 0;
 uint8_t first_detection = 0;
 
 
 ISR (TIM0_COMPA_vect)
 {
 	counter++;
-
 }
 
 void setup_cycles(){
@@ -66,7 +73,6 @@ void stop_program(){
 	while(1){
 		PORTB = no_lever;
 	}
-
 }
 
 void get_inputs(){
@@ -75,18 +81,25 @@ void get_inputs(){
 	//and shifting bits to the right
 	
 	uint8_t input = PINB;
-		
+	
+	//mask out the lever-bits with fixed variables	
 	lever = input & lever_mask;
 	lever = lever >> 2;
 	
 	if (!first_detection)
 	{
 		first_detection = lever;
-	}
+		
+		//If the first-detection is not both, set the state for the outputs from the first-detection
+		if(first_detection != both){
+			state = first_detection;	//Set the lever-state
+			time_first_detection = counter;
+		}else{
+			comfort = disabled;			//disabling the comfort-mode 
+		}
 	
 	Feedback = input & feedback_pin;
 	Feedback = Feedback >> 4;
-	detect_count++;
 }
 
 int main(void)
@@ -106,7 +119,6 @@ int main(void)
 	//read flash-cycles from memory 
 	cycles = eeprom_read_byte((uint8_t*)16);
 	
-		
 	while (1) 
     {
 
@@ -114,6 +126,7 @@ int main(void)
 		
 		//Anti-bounce: only check after 2ms if feedback signal has changed
 		if(counter - counter_last_check > 2){
+			
 			//check if the signal has a falling edge
 			if (Feedback != Feedback_last && !Feedback)
 			{
@@ -121,75 +134,49 @@ int main(void)
 				Feedback_count++;
 				//save the last counter-state for the last feedback-edge
 				time_last_Feedback = counter;
-				
 			//Check if the last feedback was too long ago
-			}else if(counter - time_last_Feedback > time_out_Feedback){
+			}else if(comfort_start_time - time_last_Feedback > time_out_Feedback){
 				stop_program();
 			}
-			//stop the program if nothing happened until time-out except both 
-			if (!comfort && counter >= time_out && lever != both)
+			
+			//disable the comfort-mode if there is no comfort-trigger detected 
+			if (comfort == inactive && time_first_detection >= time_out)
 			{
-				stop_program();
+				comfort = disabled;
+			}
+			
+			if (comfort == active)
+			{
+				//Stop the program if both lever are detected for specific times
+				if(lever == both) both_counter + 2;
+				else both_counter = 0;
+				
+				if (both_counter > time_out_both)
+				{
+					stop_program();
+				}
+				
+			}else if (first_detection == both)
+			{
+				//If both levers detected at the beginning and no comfort-mode, setup cycles
+				setup_cycles();
 			}
 			
 			Feedback_last = Feedback;
 			counter_last_check = counter;
 		}
-		switch (lever)
-		{
-			case no_lever:				//no lever is set
-				
-				if(!comfort && counter >= time_in){
-					//check if the lever was released in time and was not hold too short
-					PORTB = state;			//Set output from the last known lever state
-					comfort = active;		//member the comfort-state
-					counter = 0;			//reset the counter for the blink-cycle
-				}
-				break;
+
+		if(lever == no_lever){			//no lever is set
 			
-			case right:
-				
-				//Only the first known state changes the state value
-				if (detect_count == 1)
-				{
-					state = right;		//hold the lever-state	
-					both_counter = 0;
-				}
-				
-				break;
-					
-			case left:
-					
-				//Only the first known state changes the state value
-				if (detect_count == 1)
-				{
-					state = left;			//hold the lever-state
-					both_counter = 0;
-				}
-				
-				break;
-					
-			case both:
-				
-				if (comfort)
-				{
-					//Stop the program if both lever are detected for specific times
-					both_counter++;
-					if (both_counter > time_out_both)
-					{
-						stop_program();
-					}
-					
-				}else if (first_detection == both)
-				{
-					//If both levers detected at the beginning and no comfort-mode, setup cycles
-					setup_cycles();
-				}
-				break;
-				
-			}
+			if (comfort == inactive && time_first_detection >= time_in){
+				//check if the lever was released in time and was not hold too short
+				PORTB = state;			//Set output-state from the first-detection
+				comfort = active;		//member the comfort-state
+				comfort_start_time = counter;
+			}		
+		}
 		
-		if (Feedback_count >= cycles && comfort)
+		if (Feedback_count >= cycles && comfort == active)
 		{
 			//stop after saved times of feedbacks
 			stop_program();
